a407f95e14c8fc56511e5ba8452598926e22eafa
Karol Struniawski <struniawski.karol@gmail.com>
2018-11-04 18:07:49 +0100

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Karol Struniawski on 04/11/2018.
+//  Copyright © 2018 Karol Struniawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
